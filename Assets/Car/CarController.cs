63131eb5077bac3d3e91bd0290e1c18525d7afa2
superspeeder <andy@delusion.org>
2025-10-22 10:57:10 -0700

    add car

--- Assets/Car/CarController.cs
+++ Assets/Car/CarController.cs
@@ -1,16 +1,62 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float FollowSpeed = 31;
+    public float TurnSpeed = 16;
+    public float KP = 1f;
+    public GameObject Player;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        StartCoroutine(toggler());
+    }
+
+    private bool run = false;
+
+    IEnumerator toggler() {
+        yield return new WaitForSeconds(1.5f);
+        run = true;
+    }
+
+    float angularError() {
+        var rot = transform.rotation.eulerAngles.y;
+        var desiredVec = Player.transform.position - transform.position;
+        var desired = MathF.Atan2(desiredVec.z, desiredVec.x);
+        return desired - rot;
+    }
+
+    float angularControl() {
+        return KP * angularError();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void FixedUpdate() {
+        if (!run) return;
+
+        rb.maxLinearVelocity = FollowSpeed;
         
+        var desiredVec = Player.transform.position - transform.position;
+        desiredVec.y = 0;
+        transform.rotation = Quaternion.LookRotation(desiredVec);
+        rb.AddRelativeForce(Vector3.forward * FollowSpeed, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Player")) {
+            // TODO: death
+        }
     }
 }
--- Assets/Level/PlayerWrapTrigger.cs
+++ Assets/Level/PlayerWrapTrigger.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class PlayerWrapTrigger : MonoBehaviour {
     public Transform ReturnLocation;
+    public GameObject Car;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -16,9 +17,13 @@ public class PlayerWrapTrigger : MonoBehaviour {
         if (other.CompareTag("Player")) {
             var cc = other.GetComponent<CharacterController>();
             cc.enabled = false;
-            other.transform.position = new Vector3(transform.position.x, transform.position.y,
-                ReturnLocation.position.z + (transform.position.z - other.transform.position.z));
+            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y,
+                ReturnLocation.position.z - (transform.position.z - other.transform.position.z));
             cc.enabled = true;
+            
+            var carpos = new Vector3(Car.transform.position.x, Car.transform.position.y, ReturnLocation.position.z - (transform.position.z - Car.transform.position.z));
+            Car.transform.position = carpos;
+            Car.GetComponent<Rigidbody>().position = carpos;
         }
     }
 }
